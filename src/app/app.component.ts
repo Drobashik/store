b7b6f76ef6d52e22984e0e0c15e4c653fc11f1ea
Vladyslav <vlad.drobot32@gmail.com>
2022-07-25 19:23:07 +0300

    Business logic, get data from backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { concatMap, concatWith, map, Observable, reduce, switchMap } from 'rxjs';
+import { TProduct } from './models/product.model';
+import { DataProductService } from './services/data-product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'store';
+export class AppComponent implements OnInit {
+
+  mouseIsOver: boolean = false;
+
+  isEnd: boolean = false
+
+  products: TProduct[] = [];
+  
+  constructor(
+    private dataProductService: DataProductService,
+    ) {}
+
+    ngOnInit(): void {
+      this.dataProductService.getProductData(10).subscribe(data => {
+        this.products = this.products.concat(data)
+      })
+    }
+
+    @HostListener("window:scroll", [])
+    onScroll(): void {
+      if ((document.documentElement.scrollTop + document.documentElement.clientHeight) >= document.documentElement.scrollHeight && !this.isEnd) {
+        this.dataProductService.getProductData(20).subscribe(data => {
+          this.products = this.products.concat(data.splice(10, 20))
+          this.isEnd = true;
+        })
+      }
+    }
 }
