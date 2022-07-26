b3f73bb82ac596e4133bfdf5e2d2571d6e9e6e34
Vladyslav <vlad.drobot32@gmail.com>
2022-07-26 11:03:53 +0300

    Added EventEmmiter to aside.ts

--- src/app/components/aside-menu/aside-menu.component.ts
+++ src/app/components/aside-menu/aside-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-aside-menu',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AsideMenuComponent implements OnInit {
 
+  @Output() valueEvent = new EventEmitter<string>()
+
+  searchValue: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+  outputEvent() {
+    this.valueEvent.emit(this.searchValue)
+  }
 }
