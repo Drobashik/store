9225bba21ea0a28efbf8585d042aad460b011480
Vladyslav <vlad.drobot32@gmail.com>
2022-07-26 11:00:32 +0300

    Added filter pipe

---
+++ src/app/pipes/filter.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { TProduct } from '../models/product.model';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(products: TProduct[], searchValue: string): TProduct[] {
+    if (!searchValue) return products
+    return products.filter(product => product.title.toLowerCase().includes(searchValue))
+  }
+
+}
